734db747155bc746cdbaf26028b0f8c75feeb32a
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-10-08 01:28:38 -0400

    love debugging on ios

--- src/client.svelte
+++ src/client.svelte
@@ -113,13 +113,13 @@
   let startedDragging: [number, number] | null = null;
   let curPos: [number, number] | null = null;
   let isSliding = false;
-  window.addEventListener("touchstart", (e) => {
+  /*window.addEventListener("touchstart", (e) => {
     if (document.activeElement?.tagName !== "INPUT") {
       isSliding = false;
       startedDragging = [e.changedTouches[0].pageX, e.changedTouches[0].pageY];
     }
-  });
-  /* window.addEventListener("touchmove", (e) => {
+  });*/
+  window.addEventListener("touchmove", (e) => {
     if (startedDragging) {
       curPos = [e.changedTouches[0].pageX, e.changedTouches[0].pageY];
       if (
@@ -134,7 +134,7 @@
         PaneMessages.style.left = `${Math.max(0, Math.min(window.innerWidth, x))}px`;
       }
     }
-  });*/
+  });
   window.addEventListener("touchend", (e) => {
     const left = Number(PaneMessages.style.left.replace("px", ""));
     LIST_COLLAPSED = left <= window.innerWidth / (LIST_COLLAPSED ? 4 : 2);
